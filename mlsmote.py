c74eb3eb845a86d8d56c3f8aa38dd000150ff73e
OmarAlaaeldein <omar.alaaeldein@ejust.edu.eg>
2024-04-28 03:41:42 -0400

    remove iloc and not use minority samples function

--- mlsmote.py
+++ mlsmote.py
@@ -7,83 +7,35 @@ from sklearn.datasets import make_classification
 from sklearn.neighbors import NearestNeighbors
 random.seed(77)
 
-def create_dataset(n_sample=1000):
-    ''' 
-    Create a unevenly distributed sample data set multilabel  
-    classification using make_classification function
-    
-    args
-    nsample: int, Number of sample to be created
-    
-    return
-    X: pandas.DataFrame, feature vector dataframe with 10 features 
-    y: pandas.DataFrame, target vector dataframe with 5 labels
-    '''
-    X, y = make_classification(n_classes=5, class_sep=2, 
-                           weights=[0.1,0.025, 0.205, 0.008, 0.9], n_informative=3, n_redundant=1, flip_y=0,
-                           n_features=10, n_clusters_per_class=1, n_samples=1000, random_state=10)
-    y = pd.get_dummies(y, prefix='class')
-    return pd.DataFrame(X), y
-
-def get_tail_label(df):
+def get_tail_label(df: pd.DataFrame, ql=[0.05, 1.]) -> list:
     """
-    Give tail label colums of the given target dataframe
-    
-    args
-    df: pandas.DataFrame, target label df whose tail label has to identified
-    
-    return
-    tail_label: list, a list containing column name of all the tail label
+    Find the underrepresented targets.
+    Underrepresented targets are those which are observed less than the median occurance.
+    Targets beyond a quantile limit are filtered.
     """
-    columns = df.columns
-    n = len(columns)
-    irpl = np.zeros(n)
-    for column in range(n):
-        irpl[column] = df[columns[column]].value_counts()[1]
-    irpl = max(irpl)/irpl
-    mir = np.average(irpl)
-    tail_label = []
-    for i in range(n):
-        if irpl[i] > mir:
-            tail_label.append(columns[i])
+    irlbl = df.sum(axis=0)
+    irlbl = irlbl[(irlbl > irlbl.quantile(ql[0])) & ((irlbl < irlbl.quantile(ql[1])))]  # Filtering
+    irlbl = irlbl.max() / irlbl
+    threshold_irlbl = irlbl.median()
+    tail_label = irlbl[irlbl > threshold_irlbl].index.tolist()
     return tail_label
 
-def get_index(df):
-  """
-  give the index of all tail_label rows
-  args
-  df: pandas.DataFrame, target label df from which index for tail label has to identified
-    
-  return
-  index: list, a list containing index number of all the tail label
-  """
-  tail_labels = get_tail_label(df)
-  index = set()
-  for tail_label in tail_labels:
-    sub_index = set(df[df[tail_label]==1].index)
-    index = index.union(sub_index)
-  return list(index)
-
-def get_minority_instace(X, y):
+def get_minority_samples(X: pd.DataFrame, y: pd.DataFrame, ql=[0.05, 1.]):
     """
-    Give minority dataframe containing all the tail labels
-    
-    args
-    X: pandas.DataFrame, the feature vector dataframe
-    y: pandas.DataFrame, the target vector dataframe
-    
     return
     X_sub: pandas.DataFrame, the feature vector minority dataframe
     y_sub: pandas.DataFrame, the target vector minority dataframe
     """
-    index = get_index(y)
+    tail_labels = get_tail_label(y, ql=ql)
+    index = y[y[tail_labels].apply(lambda x: (x == 1).any(), axis=1)].index.tolist()
+    
     X_sub = X[X.index.isin(index)].reset_index(drop = True)
     y_sub = y[y.index.isin(index)].reset_index(drop = True)
     return X_sub, y_sub
 
-def nearest_neighbour(X):
+def nearest_neighbour(X: pd.DataFrame, neigh) -> list:
     """
-    Give index of 5 nearest neighbor of all the instance
+    Give index of 10 nearest neighbor of all the instance
     
     args
     X: np.array, array whose nearest neighbor has to find
@@ -91,11 +43,11 @@ def nearest_neighbour(X):
     return
     indices: list of list, index of 5 NN of each element in X
     """
-    nbs=NearestNeighbors(n_neighbors=5,metric='euclidean',algorithm='kd_tree').fit(X)
-    euclidean,indices= nbs.kneighbors(X)
+    nbs = NearestNeighbors(n_neighbors=neigh, metric='euclidean', algorithm='kd_tree').fit(X)
+    euclidean, indices = nbs.kneighbors(X)
     return indices
 
-def MLSMOTE(X,y, n_sample):
+def MLSMOTE(X, y, n_sample, neigh=5):
     """
     Give the augmented data using MLSMOTE algorithm
     
@@ -108,30 +60,58 @@ def MLSMOTE(X,y, n_sample):
     new_X: pandas.DataFrame, augmented feature vector data
     target: pandas.DataFrame, augmented target vector data
     """
-    indices2 = nearest_neighbour(X)
+    indices2 = nearest_neighbour(X, neigh=5)
     n = len(indices2)
     new_X = np.zeros((n_sample, X.shape[1]))
     target = np.zeros((n_sample, y.shape[1]))
     for i in range(n_sample):
-        reference = random.randint(0,n-1)
-        neighbour = random.choice(indices2[reference,1:])
+        reference = random.randint(0, n-1)
+        neighbor = random.choice(indices2[reference, 1:])
         all_point = indices2[reference]
         nn_df = y[y.index.isin(all_point)]
         ser = nn_df.sum(axis = 0, skipna = True)
-        target[i] = np.array([1 if val>2 else 0 for val in ser])
+        target[i] = np.array([1 if val > 0 else 0 for val in ser])
         ratio = random.random()
-        gap = X.loc[reference,:] - X.loc[neighbour,:]
-        new_X[i] = np.array(X.loc[reference,:] + ratio * gap)
+        gap = X.iloc[reference,:] - X.iloc[neighbor,:]
+        new_X[i] = np.array(X.iloc[reference,:] + ratio * gap)
     new_X = pd.DataFrame(new_X, columns=X.columns)
     target = pd.DataFrame(target, columns=y.columns)
-    new_X = pd.concat([X, new_X], axis=0)
-    target = pd.concat([y, target], axis=0)
     return new_X, target
+
+def SMOLTE_cat_wrapper(x_df, y_df, cat_col, nsamples):
+    x_df_up = pd.DataFrame(columns=x_df.columns)
+    y_df_up = pd.DataFrame(columns=y_df.columns)
+
+    unique_cat_combs = x_df.groupby(cat_col).size().reset_index().rename(columns={0:'count'})[cat_col]
+    num_cols = x_df.columns.drop(cat_col).tolist()
+    for index, row in unique_cat_combs.iterrows():
+        condition = (x_df[cat_col] == row).all(axis=1)
+
+        subx = x_df[condition][num_cols].reset_index(drop=True)
+        suby = y_df[condition].reset_index(drop=True)
+
+        x_df_sub, y_df_sub = get_minority_samples(subx, suby)
+        a, b = MLSMOTE(x_df_sub, y_df_sub, nsamples, 5)
+        cats = pd.concat([row.to_frame().T]*len(a), ignore_index=True)
+        a = pd.merge(cats, a, how='left', left_index=True, right_index=True)
+        x_df_up = x_df_up.append(a, ignore_index=True)
+        y_df_up = y_df_up.append(b, ignore_index=True)
+    #y_df_up = y_df_up.astype(int)
+    
+    print('Number of new samples created: %d' %(len(y_df_up)))
+    
+    x_df_up = pd.concat([x_df, x_df_up], ignore_index=True)
+    y_df_up = pd.concat([y_df, y_df_up], ignore_index=True)
+    
+    x_df_up = x_df_up.sample(len(x_df_up), random_state=1881).reset_index(drop=True)
+    y_df_up = y_df_up.sample(len(y_df_up), random_state=1881).reset_index(drop=True)
+    
+    x_df_up[cat_col] = x_df_up[cat_col].astype(int)
+    return x_df_up, y_df_up
 
 if __name__=='__main__':
     """
     main function to use the MLSMOTE
     """
     X, y = create_dataset()                     #Creating a Dataframe
-    X_sub, y_sub = get_minority_instace(X, y)   #Getting minority instance of that datframe
-    X_res,y_res =MLSMOTE(X_sub, y_sub, 100)     #Applying MLSMOTE to augment the dataframe
+    X_res,y_res =MLSMOTE(X, y, 100)     #Applying MLSMOTE to augment the dataframe
